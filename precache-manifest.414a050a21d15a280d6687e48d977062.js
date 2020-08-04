a0be58f215280b02afcb08f8b14809998247f13f
Ibrahim El-bana <ibrahimaelbana@gmail.com>
2020-08-04 18:10:15 +0200

    Updates

---
+++ precache-manifest.414a050a21d15a280d6687e48d977062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3228661b15aeb134976b4b2f4ceb9d",
+    "url": "/color-project/index.html"
+  },
+  {
+    "revision": "435024bdac3759f2a887",
+    "url": "/color-project/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "c291886fa62c5315d607",
+    "url": "/color-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "435024bdac3759f2a887",
+    "url": "/color-project/static/js/2.b9a51d48.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-project/static/js/2.b9a51d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c291886fa62c5315d607",
+    "url": "/color-project/static/js/main.04dc459e.chunk.js"
+  },
+  {
+    "revision": "9f3e22d4d1ff31a5f46b",
+    "url": "/color-project/static/js/runtime-main.9d5a9684.js"
+  }
+]);
